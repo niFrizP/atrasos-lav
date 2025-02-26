28f7d784c7e4b45c09ac0311cfeaf43e9b10b69f
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2025-02-25 23:33:27 -0300

    Añadir script de Sentry para manejo de excepciones en la vista principal.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
 
 
     <!-- Scripts -->
+    <script src="https://js.sentry-cdn.com/8591134519c159280367d7a967bbea01.min.js" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Vite.js para Laravel -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> <!-- CDN para jQuery -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> <!-- CDN para Alpine.js -->
